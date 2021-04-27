418e3277fa77d8fd7897f061a2e6f6fa722a6ca5
ramirex <ramirex@gmail.com>
2021-04-26 23:07:58 -0400

    terminamos clase 5

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Componets Temp')),
       body: ListView(
-        children: [
-          ListTile(title: Text('ListTile Title')),
-          Divider(),
-          ListTile(title: Text('ListTile Title')),
-          Divider(),
-          ListTile(title: Text('ListTile Title'))
-        ],
+        children: _crearItemsCorta(),
       ),
     );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = [];
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)..add(Divider());
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item + "!"),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
   }
 }
