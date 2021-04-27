553055c0fa1086606d558ff5e1f9f12e637990d3
ramirex <ramirex@gmail.com>
2021-04-26 22:42:31 -0400

    segunda clase

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Componets Temp')),
+      body: ListView(
+        children: [
+          ListTile(title: Text('ListTile Title')),
+          Divider(),
+          ListTile(title: Text('ListTile Title')),
+          Divider(),
+          ListTile(title: Text('ListTile Title'))
+        ],
+      ),
     );
   }
 }
