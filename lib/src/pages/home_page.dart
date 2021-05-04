e8bf687edc7b08d11a50d1ff9abc6c3bccc441ad
ramirex <ramirex@gmail.com>
2021-05-03 22:05:31 -0400

    creando rutas

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
@@ -22,13 +23,13 @@ class HomePage extends StatelessWidget {
         print(snapshot.data);
 
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
     data.forEach((opt) {
       final widgetTemp = ListTile(
@@ -38,7 +39,10 @@ class HomePage extends StatelessWidget {
           Icons.keyboard_arrow_right,
           color: Colors.blue,
         ),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route);
+        },
       );
 
       opciones..add(widgetTemp)..add(Divider());
