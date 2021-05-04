b401d08e51f6cb194f83a08b5d0068d2940abeb5
ramirex <ramirex@gmail.com>
2021-05-03 21:39:26 -0400

    integrando el future para async await

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,17 +13,49 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    print(menuProvider.opciones);
-    return ListView(
-      children: _listaItems(),
+    // print(menuProvider.opciones);
+    // menuProvider.cargarData().then((opciones) {
+    //   print('_lista');
+    //   print(opciones);
+    // });
+    //
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, snapshot) {
+        print('builder');
+        print(snapshot.data);
+
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
+
+    // return ListView(
+    //   children: _listaItems(),
+    // );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(
-        title: Text('Hola Mundo'),
-      )
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(
+          Icons.account_circle,
+          color: Colors.blue,
+        ),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.blue,
+        ),
+        onTap: () {},
+      );
+
+      opciones..add(widgetTemp)..add(Divider());
+    });
+
+    return opciones;
   }
 }
