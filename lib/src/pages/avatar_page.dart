501093ee8ed525b6832af01125dc03bce8e9b92c
ramirex <ramirex@gmail.com>
2021-05-04 21:20:00 -0400

    trabajando en el avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(2.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1Zgv3IEz9vyuIVmqP7GwhdTPWioCPm9vh3A&usqp=CAU'),
+              radius: 23.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://www.maxim.com/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_1329/MTQ5NjM0ODA5OTk4NjE2Mzc4/adriana-lima.webp'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
