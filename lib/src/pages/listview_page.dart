6b3f6f972e803493e8326706634c65c857c0a58a
ramirex <ramirex@gmail.com>
2021-05-06 00:15:40 -0400

    integrando eventos del scroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -6,7 +6,22 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
-  List<int> _listaNumeros = [1, 2, 3, 4, 5];
+  ScrollController _scrollController = new ScrollController();
+  List<int> _listaNumeros = [];
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _agregar10();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _agregar10();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +34,7 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearLista() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (context, index) {
         final imagen = _listaNumeros[index];
@@ -28,5 +44,14 @@ class _ListaPageState extends State<ListaPage> {
                 NetworkImage('https://picsum.photos/500/300/?image=$imagen'));
       },
     );
+  }
+
+  void _agregar10() {
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+
+    setState(() {});
   }
 }
