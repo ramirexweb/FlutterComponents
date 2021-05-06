3ea16a20b4c2549eb83607293179265f41f364fe
ramirex <ramirex@gmail.com>
2021-05-05 22:43:42 -0400

    CAMBIAMOS EL IDIOMA

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,6 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes APP',
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [const Locale('en', 'US'), const Locale('es', 'ES')],
       initialRoute: '/',
       routes: getApplicationRoutes(),
       onGenerateRoute: (RouteSettings settings) {
